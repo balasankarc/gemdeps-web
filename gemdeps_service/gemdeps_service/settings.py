00e1f323b966b1b3fed7685110eaac6595dbd034
Balasankar C <balasankarc@autistici.org>
2016-09-16 19:40:02 +0530

    Set path for resources

--- gemdeps_service/gemdeps_service/settings.py
+++ gemdeps_service/gemdeps_service/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'webgemdeps'
 ]
 
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.abspath(os.path.join(BASE_DIR,
+                                          'webgemdeps', 'resources/'))
--- gemdeps_service/gemdeps_service/urls.py
+++ gemdeps_service/gemdeps_service/urls.py
@@ -1,9 +1,20 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.views.static import serve
 
-from webgemdeps.views import StaticPages
+from webgemdeps.views import StaticPages, UserManagement
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', StaticPages.Index.as_view()),
+    url(r'^sign_in/$',
+        UserManagement.UserSignIn.as_view()),
+    url(r'^sign_out/$',
+        UserManagement.UserSignOut.as_view()),
+    url(r'^home/$',
+        UserManagement.UserHome.as_view()),
+    url(r'^resources/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT, }),
 ]
