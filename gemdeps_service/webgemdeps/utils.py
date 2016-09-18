7afd5a33f27b9cdbe51d5243eb25f046ef3b3e94
Balasankar C <balasankarc@autistici.org>
2016-09-18 13:29:26 +0530

    Split getting resource path

--- gemdeps_service/webgemdeps/utils.py
+++ gemdeps_service/webgemdeps/utils.py
@@ -4,9 +4,7 @@ import os
 
 
 def get_available_apps():
-    APP_PATH = os.path.dirname(os.path.dirname(os.path.abspath(
-        __file__)))
-    RESOURCE_PATH = os.path.join(APP_PATH, 'webgemdeps', 'resources')
+    RESOURCE_PATH = get_resource_path()
     print RESOURCE_PATH
     directories = [os.path.join(RESOURCE_PATH, x) for x in
                    os.listdir(RESOURCE_PATH) if
@@ -17,7 +15,16 @@ def get_available_apps():
         deb_status_path = os.path.join(directory, 'debian_status.json')
         if os.path.isfile(gemfile_lock_path) and \
                 os.path.isfile(deb_status_path):
-            name, version = os.path.basename(directory).split(
-                '_')
+            basename = os.path.basename(directory)
+            position = basename.index('-')
+            name = basename[:position]
+            version = basename[position + 1:]
             apps.append({'name': name, 'version': version})
     return apps
+
+
+def get_resource_path():
+    APP_PATH = os.path.dirname(os.path.dirname(os.path.abspath(
+        __file__)))
+    RESOURCE_PATH = os.path.join(APP_PATH, 'webgemdeps', 'resources')
+    return RESOURCE_PATH
