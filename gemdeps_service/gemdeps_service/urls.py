697bdadfeaa434cd7b722363634c0c9d567b635e
Balasankar C <balasankarc@autistici.org>
2016-09-18 13:28:32 +0530

    Add url to create status

--- gemdeps_service/gemdeps_service/urls.py
+++ gemdeps_service/gemdeps_service/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.views.static import serve
 
-from webgemdeps.views import StaticPages, UserManagement
+from webgemdeps.views import StaticPages, UserManagement, StatusManagement
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -12,6 +12,8 @@ urlpatterns = [
         UserManagement.UserSignIn.as_view()),
     url(r'^sign_out/$',
         UserManagement.UserSignOut.as_view()),
+    url(r'^new_status/$',
+        StatusManagement.StatusCreate.as_view()),
     url(r'^home/$',
         UserManagement.UserHome.as_view()),
     url(r'^resources/(?P<path>.*)$',
