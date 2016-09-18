02cd20651cf3383cd54d4c212748bb14a32729ff
Balasankar C <balasankarc@autistici.org>
2016-09-18 13:26:07 +0530

    Add form for creating new status bar

--- gemdeps_service/webgemdeps/forms.py
+++ gemdeps_service/webgemdeps/forms.py
@@ -5,3 +5,10 @@ from django.contrib.auth.models import User
 class SignInForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField()
+
+
+class NewStatusForm(forms.Form):
+    appname = forms.CharField()
+    version = forms.CharField()
+    gemfile = forms.FileField()
+    gemfilelock = forms.FileField()
