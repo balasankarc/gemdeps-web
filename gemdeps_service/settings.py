4ffc08931859cf3d7e5a0921549f563bb88b77b7
Balasankar C <balasankarc@autistici.org>
2016-09-18 18:40:39 +0530

    Add redis to requirements

--- gemdeps_service/settings.py
+++ gemdeps_service/settings.py
@@ -124,3 +124,12 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.abspath(os.path.join(BASE_DIR,
                                           'webgemdeps', 'resources/'))
+
+
+# CELERY STUFF
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Asia/Kolkata'
