5d51f5a1bc67904bacd5e0d719d6870f966be78d
Balasankar C <balasankarc@autistici.org>
2016-09-18 18:40:12 +0530

    Add model for job

--- webgemdeps/models.py
+++ webgemdeps/models.py
@@ -2,3 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+
+
+class Job(models.Model):
+    appname = models.CharField(max_length=100)
+    job_id = models.CharField(max_length=100)
