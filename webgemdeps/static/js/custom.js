95faae91382e44617a3f4479719e2155e58d4eb3
Balasankar C <balasankarc@autistici.org>
2016-09-24 19:28:02 +0530

    Remove redundant alert

--- webgemdeps/static/js/custom.js
+++ webgemdeps/static/js/custom.js
@@ -1,6 +1,5 @@
 function deleteConfirm(appname) {
     if (confirm('Do you want to delete ' + appname + '?')) {
-        alert(appname);
         window.location.href = "/delete/" + appname;
     } else {
         return false;
